f595f77fce5751cbd9d0239b0f395db802f3037d
sirappleford <83780605+sirappleford@users.noreply.github.com>
2021-07-06 19:22:18 +0100

    made pc_turn function with pc_attack function

--- Ice spire peak text adventure/combat.py
+++ Ice spire peak text adventure/combat.py
@@ -1,13 +1,20 @@
 import attacks
 import character_creation
 import random
+from attacks import pc_attack
+import character_creation
+def pc_turn():
+    will_pc_attack = input("its your turn of the comabt would you like to attack with "+character_creation.weapon+" to attack say attack")
+    if will_pc_attack == "attack":
+        pc_attack()
+
 if enemy_attitude == "hostile":
     fight_start = 1
 if fight_start == 1:
     character_creation.pc_initiative = random.randint(1,20)+character_creation.DexterityMod
     enemy_initiative = random.randint(1,20)+enemy_DexterityMod
     if character_creation.pc_initiative > enemy_initiative:
-        pc_turn = 1
+        pc_turn()
         enemy_turn = 2
     elif enemy_initiative > character_creation.pc_initiative:
         pc_turn = 2
