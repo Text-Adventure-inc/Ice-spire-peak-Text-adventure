48febc64e46d68e5c47eb5eca0767d5ee7d46122
sirappleford <83780605+sirappleford@users.noreply.github.com>
2021-07-17 13:47:10 +0100

    connected enemy dex to the file

--- Ice spire peak text adventure/combat.py
+++ Ice spire peak text adventure/combat.py
@@ -3,6 +3,7 @@ import character_creation
 import random
 from attacks import pc_attack
 import character_creation
+import enemy_stats 
 def pc_turn():
     will_pc_attack = input("its your turn of the comabt would you like to attack with "+character_creation.weapon+" to attack say attack")
     if will_pc_attack == "attack":
@@ -12,7 +13,7 @@ if enemy_attitude == "hostile":
     fight_start = 1
 if fight_start == 1:
     character_creation.pc_initiative = random.randint(1,20)+character_creation.DexterityMod
-    enemy_initiative = random.randint(1,20)+enemy_DexterityMod
+    enemy_initiative = random.randint(1,20)+enemy_stats.enemy_DexterityMod
     if character_creation.pc_initiative > enemy_initiative:
         pc_turn()
         enemy_turn = 2
